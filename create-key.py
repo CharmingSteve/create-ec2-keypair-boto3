5d80e976e08a786e6435fcd2a7862d444e8b1aad
Steve Bar Yakov Gindi <steve@milknhoney.co.il>
2018-06-28 16:16:47 +0300

    Update create-key.py
    Creates an AWS key pair with the name of the present region. You can use it to connect to Ec2 that you create after, via ssh or RDP for Windows

--- create-key.py
+++ create-key.py
@@ -2,10 +2,14 @@
 
 import boto3
 import sys
-nameofkey = sys.argv[1]
-outfile = open((nameofkey)+'.pem','w')
-ec2 = boto3.client('ec2')
-keypair = ec2.create_key_pair(KeyName=(nameofkey))
+import urllib.request
+import os
+f = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/placement/availability-zone/')
+region = (f.read(100).decode('utf-8')[:-1])
+outfile = open((region)+'.pem','w')
+ec2 = boto3.client('ec2', region_name=(region))
+keypair = ec2.create_key_pair(KeyName=(region))
 i = keypair['KeyMaterial']
 outfile.write(i)
 outfile.close()
+os.chmod((region)+'.pem', 400)
