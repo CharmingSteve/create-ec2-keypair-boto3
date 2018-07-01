8caa68858d3788db8d3065707c7be2c44bc20d8a
Steve Bar Yakov Gindi <steve@milknhoney.co.il>
2018-07-01 16:49:01 +0300

    Update create-key.py
    changed to use the cli args

--- create-key.py
+++ create-key.py
@@ -2,10 +2,14 @@
 
 import boto3
 import sys
-import urllib.request
 import os
-f = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/placement/availability-zone/')
-region = (f.read(100).decode('utf-8')[:-1])
+
+
+region = sys.argv[1]
+#use the following to call the region from the script instead of typing in as arg.
+#import urllib.request
+#f = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/placement/availability-zone/')
+#region = (f.read(100).decode('utf-8')[:-1])
 outfile = open((region)+'.pem','w')
 ec2 = boto3.client('ec2', region_name=(region))
 keypair = ec2.create_key_pair(KeyName=(region))
